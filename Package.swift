eebcf56fa29dd0645d16de49fb2e1f78cc7085e3
moguaiyuki <moguai.yuki0424@gmail.com>
2019-04-07 17:36:42 +0900

    swift SMTP install

--- Package.swift
+++ Package.swift
@@ -14,14 +14,15 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/Kitura-CORS.git", from: "2.1.0"),
         .package(url: "https://github.com/IBM-Swift/Kitura-OpenAPI.git", from: "1.0.0"),
         .package(url: "https://github.com/IBM-Swift/Swift-Kuery-ORM", from: "0.4.1"),
-        .package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL", from: "2.1.0")
+        .package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL", from: "2.1.0"),
+        .package(url: "https://github.com/IBM-Swift/Swift-SMTP", .upToNextMinor(from: "5.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "api",
-            dependencies: ["Kitura" , "HeliumLogger", "SwiftKueryPostgreSQL", "SwiftKueryORM", "CloudEnvironment","SwiftMetrics", "Health", "KituraOpenAPI", "KituraCORS"],
+            dependencies: ["Kitura" , "HeliumLogger", "SwiftKueryPostgreSQL", "SwiftKueryORM", "CloudEnvironment","SwiftMetrics", "Health", "KituraOpenAPI", "KituraCORS", "SwiftSMTP"],
             path: "Sources"),
         .testTarget(
             name: "apiTests",
